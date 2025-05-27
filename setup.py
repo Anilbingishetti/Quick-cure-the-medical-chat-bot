0f1f467245487c47aa5a62772ec113315d8dbf43
Bingishetti Anil kumar <bingishettianilkumar23@gmail.com>
2025-05-27 09:38:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ='chat bot',
+    version = '0.0.0',
+    author = 'Anil Kumar',
+    author_email = 'bingishettianilkumar23@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+    
+)
